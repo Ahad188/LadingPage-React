bbfd2ffafa360a2fed0afb6800e5d23d8f9157e4
AhaD <abuahad35188@gmail.com>
2023-09-24 17:24:07 +0600

    footer r cat

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Achievement from './Components/Achievement/Achievement'
 import Categories from './Components/Categories/Categories'
 import Companies from './Components/Companies/Companies'
 import Coerces from './Components/Courses/Cources'
+import Cta from './Components/Cta/Cta'
+import { Footer } from './Components/Footer/Footer'
 import Hero from './Components/Hero/Hero'
 import Navbar from './Components/Navbar/Navbar'
 
@@ -18,6 +20,8 @@ function App() {
       <Coerces/>
       <Achievement/>
       <Categories/>
+      <Cta/>
+      <Footer/>
     </>
   )
 }
